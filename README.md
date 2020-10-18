42b24e0d610aeecbb75bfd5459dfaebc996337e9
Dating Libre <admin@datinglibre.com>
2020-10-18 07:18:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Credits
+
+The `countries.sql` `regions.sql` and `cities.sql` files were created by processing geographical data from [GeoNames](https://www.geonames.org/).
---
+++ countries.sql
@@ -0,0 +1,262 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+COPY datinglibre.countries (id, name) FROM stdin;
+b0ea9087-7991-4f27-b2c8-512eb95c5078	Afghanistan
+01637b73-f921-49a9-ad39-0bed17f5d817	Aland Islands
+987112e8-fe7e-4d30-a5bb-35d529eb7ddd	Albania
+f63d1e37-f519-4b39-9241-0bcf3e263d33	Algeria
+d82ab0eb-7c18-4f26-a095-b9ca6ce0e753	American Samoa
+b65d1544-5c35-47f7-84e2-2b2b2bec94d4	Andorra
+6acc7b05-b218-482b-95d0-cbd3eb223f3b	Angola
+6e59c54d-4f8e-4e28-a572-4a90486ce31c	Anguilla
+8de71ee8-9aa8-4e99-9dac-aeb8a8fc1219	Antigua and Barbuda
+58c6dadc-b96c-4a36-a3e0-481433a07881	Argentina
+70b32182-3179-4732-93f9-a4d106bc2136	Armenia
+9907f397-440c-4ee2-9415-b7610e99cbcd	Aruba
+41b3e8a1-7489-459b-a447-41e543b931a1	Australia
+3b012f0a-9738-4eed-a712-6ad2a82f44a6	Austria
+2e54fbd9-a8db-4920-8c4a-ad75e5d93741	Azerbaijan
+0d50830b-45b3-4d88-84b2-c9b379ad6534	Bahamas
+ee601ab3-91fb-4670-bde2-2239290faf3b	Bahrain
+bd7d2bf7-b217-4993-a832-fe9832cc97d5	Bangladesh
+98852a47-ee97-4b49-a04f-7df438850231	Barbados
+d079d12c-38f8-425b-836e-c769bf676d1f	Belarus
+2f0c54f0-18c0-46f0-ab93-65a624b8ab4c	Belgium
+c9404b89-f886-478d-8d23-39b679d25aff	Belize
+8fa23951-7985-409b-ba1a-4a670f9c737d	Benin
+40767a52-d1a0-496a-873f-6d995fc13c4f	Bermuda
+2db1d0df-1e7f-4a55-b761-45ff4c443c55	Bhutan
+4fa83a7f-c526-4d14-bf48-672855a05693	Bolivia
+5014eb43-3d7f-4f84-97c8-a45f3128520f	Bonaire, Saint Eustatius and Saba
+91078b6d-68cc-4487-9d7c-bc8d38e349af	Bosnia and Herzegovina
+b66d9182-08b5-4c04-b4f5-b3aed05afb3c	Botswana
+0138ff27-7c08-440d-be79-1fbac69a8912	Brazil
+d9307009-2986-4632-befe-756306bd0da3	British Virgin Islands
+f9e15a44-886f-4c06-a2ed-72398e9e09d0	Brunei
+46a12811-8d16-47dc-993f-652ea6b48b06	Bulgaria
+9fdfd956-f272-49ea-a1b0-7cb748d27fc4	Burkina Faso
+870ba54b-3a0d-409a-a5c3-13b13077f89d	Burundi
+bdf4130d-bb93-4f97-a97e-91b9076acc07	Cambodia
+5db815c0-1069-48d9-b747-47de9a11e691	Cameroon
+341cc0e5-607f-44e7-aee5-549095daba40	Canada
+800960f6-b85d-4b77-af73-cc8b8694b1ff	Cape Verde
+29d2b978-0241-449c-b6ef-3abd653002a7	Cayman Islands
+36834896-1335-48ce-aba9-15a65ceac526	Central African Republic
+77da918b-052e-49fa-80ed-2cd6b2c0bfc6	Chad
+1a6c4664-44ff-4050-8942-2bb8b570f99f	Chile
+bf3068d3-c0bd-4655-a3ba-00e93c18c312	China
+1f216d15-c943-4b0b-83c1-4f68247cae7f	Christmas Island
+a1144934-0689-46a0-bac8-df7f2d059a7f	Cocos Islands
+713f0b24-9eb5-4428-933d-e8085fa88404	Colombia
+e8e57ad7-460c-4bc5-b90b-34f0afd273dd	Comoros
+d1c550c3-5579-4589-aae3-104dc277482d	Cook Islands
+b9c742fa-d752-48a1-9220-2c14ce6a519d	Costa Rica
+9048b269-e918-476f-8e4f-e3bc34d3f454	Croatia
+10749aac-3b0e-4f88-94a9-04764a259c7f	Cuba
+da2226a8-ce8b-43cb-a877-db35cc09e9f4	Curacao
+5818e9fb-b9b8-463e-bd23-8827d58f3567	Cyprus
+de95dda2-b04f-4bb8-8121-a529bf61af91	Czechia
+8696c966-2fdf-4884-ade6-aa55a49ba226	Democratic Republic of the Congo
+97898abf-abe5-4b1c-b4a2-29b861f447a8	Denmark
+eab7a8e6-b916-4899-89aa-b72e9e3d16c6	Djibouti
+3d860cb7-8593-4526-b7b6-ba9b364b9b00	Dominica
+b09878ad-362e-47c5-8ad8-53913af15e00	Dominican Republic
+afe27ca9-3635-4f1a-84bf-d988fd6b1c37	East Timor
+5b35b89a-620c-4a36-9189-d46a4cecb176	Ecuador
+bd43e0d5-9c60-4e7f-a84b-2bf4bfa4f01e	Egypt
+e8e877c0-d088-4143-bb8f-13c1bfacbdcd	El Salvador
+acbe302a-7ccd-4fb9-a0b8-de5e05bb6e4b	Equatorial Guinea
+0747cbf8-95b6-4f21-82f7-28e01d96b50c	Eritrea
+e8220605-cb57-4ce2-8ef6-8cfdc2db005e	Estonia
+fd9c0c9d-a54f-412e-b025-407ebfc4286c	Ethiopia
+52cd93ff-2a43-409e-8405-e1df65cd2267	Falkland Islands
+6767099c-63c5-4cb6-b57a-001e3fd04963	Faroe Islands
+8152527c-6a43-41ab-97af-7ed1a4e6ee4c	Fiji
+0dffa40f-623d-4ba3-9bd0-73e49365b673	Finland
+67c53ed4-1556-4e23-b45c-b91b8e8998a1	France
+163efbf8-6da4-4d8d-83ce-30c5df14a375	French Guiana
+c761d797-c87d-4611-aa64-6437a5425280	French Polynesia
+e97e041d-4367-4295-9480-aef491168032	French Southern Territories
+bb803c0d-5b20-45c6-8e35-28201ca1a125	Gabon
+ff8744c5-4a66-40e3-b909-b84235da0add	Gambia
+d7048da3-5258-4734-9920-8be4b0ecd600	Georgia
+12139191-2bb5-48a1-9b9d-c2e814067339	Germany
+b090097d-026a-4431-8805-ae4f5c606282	Ghana
+cc661d46-83cf-4176-af75-a02883be60ba	Gibraltar
+366cf941-1de1-4431-a860-1d7406d9c835	Greece
+42109a47-c94a-4db3-b9a3-66dad6904151	Greenland
+2d33cbed-eca0-4c9f-a0a6-aab96085a2e9	Grenada
+2d0930f9-852c-404b-87b4-aa47b1830594	Guadeloupe
+e6c05674-2b27-462f-bb65-87816d550ef4	Guam
+5eeee96e-8722-4a6d-a4c3-65b5af924cc1	Guatemala
+cec8d6fc-f5e5-45ad-9c92-a2ee510af2ad	Guernsey
+9103dd57-787e-49bb-be6a-0dc3d5a000de	Guinea
+dc42ea57-6e2e-4d75-8f2b-d3b8c614cd42	Guinea-Bissau
+0c7ade06-d06b-4174-8185-8127c9573395	Guyana
+34cb3d14-bebe-43bc-87fd-122f540e6e87	Haiti
+9f5b3355-a570-4cbc-ad78-f4a803ed6614	Honduras
+cfe69a6d-2cdf-495c-ad9d-4d7a71d148f0	Hong Kong
+88de37a8-b2c9-4097-8a37-7583c6c68498	Hungary
+e78390ca-6f83-4d8e-bc2b-03a6c33ae221	Iceland
+e67bfb4b-0301-4f6d-b430-73cc45088b62	India
+d30e46b9-fde3-4586-86c6-950a35803f56	Indonesia
+139d747b-2c22-4ae7-893e-d879c012f4f0	Iran
+f64ac147-bbd3-4755-9937-ef9c5759967f	Iraq
+4f1d27ee-5c7e-40ff-936a-9e17eefcd02c	Ireland
+2f272123-4a6b-4f4c-9dbe-287ab3895b70	Isle of Man
+5e2b42e4-d858-4f5f-b374-11de80d5b99c	Israel
+2fb432fa-b8d2-4866-a94a-d05c05a738cd	Italy
+18be60fd-fd67-4708-8f6e-5aac1286190b	Ivory Coast
+430fc49a-ccf4-44de-81ac-1b05333aa871	Jamaica
+63defeb2-0840-4288-a100-adc4693b3abc	Japan
+970c798b-5008-4e98-aec1-1c1cc5c1b7df	Jersey
+5b61a90e-e7c6-4923-8be2-f45f6b7ba8f3	Jordan
+141501cd-fb29-4de0-8f99-400233077267	Kazakhstan
+564dac77-c8f0-4f7d-abe2-7bd92e98b945	Kenya
+e9cfbfeb-6a51-4e44-869d-d615e3895da3	Kiribati
+f800378d-6846-4cbe-aa5f-4c0e9def7f05	Kosovo
+cefac58d-de5a-49a4-842a-83d1b8720af7	Kuwait
+d159672a-9eba-4dcc-8ddf-8b048370aff3	Kyrgyzstan
+83875b6f-240c-42e3-9088-b0c1235c892c	Laos
+2da434f4-c923-4450-846b-0bb9cfa47177	Latvia
+7d2c77c2-306a-4a53-bbe5-652bf77fad45	Lebanon
+efade68c-9aa0-4f17-b8e8-79092f30df92	Lesotho
+d8620d9d-1e88-42ad-a640-d99cc8ebe348	Liberia
+294e4f58-047d-472b-a67c-c749342bb98f	Libya
+d645a8ff-4b4f-4925-be61-d996a6e8e2a0	Liechtenstein
+4473bbd6-ba96-4275-bda3-d9c7c289bdab	Lithuania
+31a737e6-5224-4212-b9df-1e6416b4e3f3	Luxembourg
+29fadabc-ef50-4f37-b7b3-55e47c3e5430	Macao
+ae964d85-2f97-4cf1-a45c-35fd5b1018d7	Macedonia
+7d522b29-ef55-4b50-bbb4-f60d26f3e20c	Madagascar
+eb1675c2-769a-49d0-8aab-fa1841073ef0	Malawi
+e1a7e4d3-8c83-4778-92fe-bc352d204f22	Malaysia
+4236924a-1dcf-440b-ae1e-4d2f4547ffc0	Maldives
+33b1611a-da73-4fbf-b872-6a85f0de8061	Mali
+062d941c-8978-46df-af7c-fd6382fbc4dc	Malta
+7d0e0567-54b0-4d93-9a1d-c82fb7a91ccf	Marshall Islands
+7d368995-56be-4770-b67a-502c5cf42855	Martinique
+ab6ea890-f7b2-4b3b-988b-255861d0cb54	Mauritania
+1ae03f7a-4dab-4a77-9c67-7d4b7cac97a8	Mauritius
+e4fc0557-99bc-4f15-bc82-1d863cd9ddca	Mayotte
+6a1cc169-f10d-4ff5-8a41-fc085e280f89	Mexico
+0c4233cd-2cb7-4f15-a69b-23083a3c0779	Micronesia
+38d056a3-1ac0-4b3d-a083-2558a9ed4113	Moldova
+d2365b7e-6f84-4e40-a929-bbf9c6d9f172	Monaco
+48874270-91fe-464d-89d1-15f4a7aea43e	Mongolia
+cf691ca0-30f6-44f6-87ac-aaa0e2bb299b	Montenegro
+408ba2cd-ca5a-4298-9ec7-5f479a650e93	Montserrat
+062a52fe-6c65-4254-bcc0-e7fd0bf2ce7e	Morocco
+4c2d1540-27d3-45c3-bc7d-0eb475d76729	Mozambique
+17241884-b716-43e1-acc6-538f6c6a8ed1	Myanmar
+c475435c-6560-487c-9b91-cb7aea2d5361	Namibia
+a9d9424f-9ae5-4e6f-a6da-562821151a61	Nauru
+b83475de-da15-43b8-b52b-5708f5d12c83	Nepal
+c69e7279-647d-46bd-8538-2bfd26e7d4d6	Netherlands
+b29bebb7-83b0-40ba-be57-f981e46f879b	New Caledonia
+89a5d1f0-8777-4590-b197-c7e2899535cd	New Zealand
+b9e0fcb0-a8d9-4c32-9a2d-9a01e74d2f0a	Nicaragua
+245a37ab-6bb4-4662-9444-640472d975db	Niger
+d6a1997d-929a-474c-8075-875d90823f07	Nigeria
+941b98fc-e972-4827-bc42-bc2e9d9931e0	Niue
+7333b62b-d93e-48d9-839c-e4e164911ba4	Norfolk Island
+5bf4674c-03f7-44f9-a949-c71b1e476cc3	North Korea
+8c90a92d-ba8b-49e1-8a86-36a3589441c2	Northern Mariana Islands
+4b3fc80d-088f-4234-83eb-e77a5f14052e	Norway
+db5d19e6-406b-4b44-8655-ba14246cb715	Oman
+5d8725ee-47c5-4b35-82a7-b07ea62db83e	Pakistan
+de594050-6b77-4843-93d6-0de71e7eeebf	Palau
+ec38b3fb-05ce-4802-8f5f-9449679a3d72	Palestinian Territory
+031b453f-0240-453e-b483-3d32f36e5fa1	Panama
+cd634d36-c710-4267-aaef-72c01983ae57	Papua New Guinea
+2917ae64-a237-4a46-909c-38a6dc169742	Paraguay
+6bce8a4f-0a52-4d55-9c56-234ae0ee2dc9	Peru
+affe0628-6ec9-4fac-baa1-f9252c275800	Philippines
+6b33605f-6418-4f1f-9004-e7a254f10f06	Pitcairn
+252db1c1-46aa-4e44-b72f-e81efa8895e2	Poland
+ea3ac61c-6d58-4737-a5c6-a2591d2e712f	Portugal
+ed3a9fc1-d31f-437c-b63f-768b1e991e6d	Puerto Rico
+854d7146-245a-4062-a72d-d523193170fc	Qatar
+f7e1a9bb-6959-48c9-9d0b-fb08b136ad52	Republic of the Congo
+191db22c-30be-4018-b861-c23aaa0071ac	Reunion
+5ecf3087-d80d-4042-8260-cd78e5b3ed4f	Romania
+7e98b94e-c37f-4ce5-a844-e1e11fc125b8	Russia
+b29ec8ed-8d95-43eb-bc03-f1e73c118e8f	Rwanda
+1bcb8b08-b3e7-4e7c-9132-191c76c31162	Saint Barthelemy
+21f0426d-5b4f-4cc3-8652-c4fd9e16f7c7	Saint Helena
+ee86ab88-84b0-4aa0-8fec-5d557b0bd8de	Saint Kitts and Nevis
+cdfa1af3-ade9-4966-9ddb-46e5b0a52b74	Saint Lucia
+c7bd3394-166b-429d-9a11-c3801d70159c	Saint Martin
+78b78bfe-c2d6-4768-8cd4-887bf629fbdc	Saint Pierre and Miquelon
+cf6a0cd6-d346-4d1b-8769-e874038086d6	Saint Vincent and the Grenadines
+a0854aca-73b5-43f7-91ae-78b2e0f11f49	Samoa
+7e827912-6327-44fc-8c3e-35fb3c03b787	San Marino
+1f7109dd-6dee-496d-bd86-29a33d71c0d7	Sao Tome and Principe
+d6b245fb-5cce-4755-a86d-0f276befbb87	Saudi Arabia
+be6b20c2-5e49-4d1a-be22-0dc003320f92	Senegal
+8d443a37-52bc-465a-adcb-6006af05fb41	Serbia
+2d54b74d-9695-4d6b-8e0a-e5b6f0fc1028	Seychelles
+86ad89f1-e4b5-4482-a499-ccf21adfee1b	Sierra Leone
+39cb1e84-59b5-47e5-802d-516636f21395	Singapore
+80e726a2-b15c-4292-bd94-6743141686dc	Sint Maarten
+b0a73a05-1a00-415e-8b37-93e3fb763af9	Slovakia
+4b674d31-2eaf-4a60-b118-4ec76ae49507	Slovenia
+a96dcc42-e784-4dc1-a67c-98d965db282b	Solomon Islands
+4f0a92b3-5a0b-4974-920c-99bd485a0583	Somalia
+7b126ae0-bcdc-4d22-ba2b-0490e8d409d0	South Africa
+9b9b96e5-1bde-413f-9809-f6e989c04442	South Georgia and the South Sandwich Islands
+02faa399-fde8-4e82-8227-ace4f1b712d2	South Korea
+73a44f41-7d49-4a0c-b749-c79bf78c6b01	South Sudan
+4fa11131-7c6a-465c-8a69-58a90aedcfa4	Spain
+a3e412af-6266-4864-96f1-aee5b8f85c1d	Sri Lanka
+2534c2f7-23f5-46db-ac50-eede6be77b68	Sudan
+69ba1ae5-a8de-4b85-ae4f-58e5a9c5713d	Suriname
+94061fb2-4570-424b-89a0-7cb71b56bb6d	Svalbard and Jan Mayen
+791d709f-fae2-4dc0-acd8-ab5ea8eaaa05	Swaziland
+6655f14d-3fc9-4e07-9ab6-48704ecb684e	Sweden
+a8d16b72-44d9-4f55-977e-63951c12b666	Switzerland
+6f71955c-efa1-4a0b-b61d-e79bcf4823f7	Syria
+cc2c9c93-1135-4f81-9b06-6affbd919305	Taiwan
+7f940856-9baa-40a7-b1bb-2915291e1b07	Tajikistan
+5cb0a013-2c8d-4f59-8cf2-f2b2f0256c99	Tanzania
+af8e3782-87c5-4f69-8c57-045818261dd4	Thailand
+36b90a84-95a1-4725-b8ea-dba6c6db79c1	Togo
+87699b1f-6993-4ab1-b087-d1ce4c56fc8e	Tonga
+dcd2961c-aec2-4d8c-a5ef-6b73350e2660	Trinidad and Tobago
+f53dc27b-94ca-41e2-a82d-f6670069e2a9	Tunisia
+01682a5f-a46b-4c1d-93a6-c96d71c75cf9	Turkey
+19f0a93c-fbba-464f-b676-3d27569cb54a	Turkmenistan
+946285d9-b7aa-4b2f-82fa-1c4e29d3f28c	Turks and Caicos Islands
+be87fae3-91bf-4d8c-b174-42f0db133d50	Tuvalu
+56088e5f-d659-44ce-a89a-30522de9640e	U.S. Virgin Islands
+5030421b-a755-41cd-9599-389a54183c4e	Uganda
+571d03ae-8824-4d26-bbe5-2a5a84a5f67c	Ukraine
+ac462313-f312-481a-a281-437a86fdddc2	United Arab Emirates
+9ae4610f-4bb2-4fd6-808e-ab48505feb82	United Kingdom
+74ea9d81-aad1-48b7-94b8-98be73d8ec1a	United States
+19501392-b5de-4eaa-9622-fa062c094fcb	Uruguay
+d7afee3e-ae43-4448-b469-9f819682e534	Uzbekistan
+f881470e-ca58-4f29-b9e4-236eb0784ffc	Vanuatu
+817a75a2-241f-48c0-8e28-fdd4c08aa5f2	Vatican
+3af8e706-182b-4892-b1b3-a3f3412a66ef	Venezuela
+a724ca8d-16de-45df-bf1f-2d4177289acf	Vietnam
+ed302a0f-17d0-487c-8791-f2230f994ae9	Wallis and Futuna
+99b417c8-22e2-4f56-b6ab-e2a821764d98	Western Sahara
+eb4b8065-4e67-4903-b8d9-886cb6e8c183	Yemen
+4e841212-0424-4b8d-821f-b154b909a990	Zambia
+5996fe2d-2c63-4ca9-a84e-a4897a6db07f	Zimbabwe
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
